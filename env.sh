395c7dd33d11b26eb14df39540c2656fa53038bb
Jack Davine <jackdavine47@gmail.com>
2023-12-26 11:44:53 +0800

    update env with riscv-dv and changbai

--- env.sh
+++ env.sh
@@ -4,4 +4,9 @@ export PATH=$YUANQI_HOME/scripts:$PATH
 mkdir -p $YUANQI_HOME/apps/riscv
 export RISCV=$YUANQI_HOME/apps/riscv
 export PATH=$RISCV/bin:$PATH
-export RISCV_TESTS_ROOT=$YUANQI_HOME/apps/riscv/
+export RISCV_TESTS_ROOT=$RISCV
+# riscv-dv
+export RISCV_TOOLCHAIN=$RISCV
+export RISCV_GCC=$RISCV_TOOLCHAIN/bin/riscv64-unknown-elf-gcc
+export RISCV_OBJCOPY=$RISCV_TOOLCHAIN/bin/riscv32-unknown-elf-objcopy
+export SPIKE_PATH=$RISCV/bin/spike
