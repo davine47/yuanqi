9cc7afabc07a1c8f483d92be4465c9e214e44a49
davine47 <davine.plus@gmail.com>
2024-02-16 04:48:55 +0800

    update

--- env.sh
+++ env.sh
@@ -10,3 +10,6 @@ export RISCV_TOOLCHAIN=$RISCV
 export RISCV_GCC=$RISCV_TOOLCHAIN/bin/riscv64-unknown-elf-gcc
 export RISCV_OBJCOPY=$RISCV_TOOLCHAIN/bin/riscv32-unknown-elf-objcopy
 export SPIKE_PATH=$RISCV/bin/spike
+export FIRRTL_PATH=$YUANQI_HOME/apps/firtool-1.65.0
+export PATH=$FIRRTL_PATH/bin:$PATH
+
