097cfb70fd3b35ccb75d23083184c1f6197396e3
davine47 <davine.plus@gmail.com>
2024-02-18 04:08:59 +0800

    update

--- env.sh
+++ env.sh
@@ -3,6 +3,7 @@ alias yq='controller.pl'
 export PATH=$YUANQI_HOME/scripts:$PATH
 mkdir -p $YUANQI_HOME/apps/riscv
 export RISCV=$YUANQI_HOME/apps/riscv
+export APPS=$YUANQI_HOME/apps
 export PATH=$RISCV/bin:$PATH
 export RISCV_TESTS_ROOT=$RISCV
 # riscv-dv
@@ -12,4 +13,5 @@ export RISCV_OBJCOPY=$RISCV_TOOLCHAIN/bin/riscv32-unknown-elf-objcopy
 export SPIKE_PATH=$RISCV/bin/spike
 export FIRRTL_PATH=$YUANQI_HOME/apps/firtool-1.65.0
 export PATH=$FIRRTL_PATH/bin:$PATH
+export PATH=$APPS/bin:$PATH
 
